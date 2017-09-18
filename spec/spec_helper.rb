e559900980c91cb14e5f692a868950ee9c4c83e5
Eric Putnam <putnam.eric@gmail.com>
2017-09-18 10:34:38 -0700

    (FM-6406) add tests
    This adds some basic tests. They're a little lack luster but what can ya do

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,25 @@
+require 'puppet_pot_generator'
 require 'puppet'
+require 'puppet/pops/parser/evaluating_parser'
+
+LOCALES_DIR = File.join(File.dirname(__FILE__), '../locales')
+
+def create_dirs(dirs)
+  puts "Creating directories: #{dirs}"
+  dirs.each { |dir| FileUtils.mkdir_p(dir) }
+end
+
+def destroy_dirs(dirs)
+  puts "Destroying directories: #{dirs}"
+  dirs.each { |dir| FileUtils.rm_rf(dir) if File.exist? dir }
+end
+
+RSpec.configure do |c|
+  c.before(:all) do # rubocop:disable RSpec/BeforeAfterAll
+    create_dirs([LOCALES_DIR])
+  end
+
+  c.after(:all) do # rubocop:disable RSpec/BeforeAfterAll
+    destroy_dirs([LOCALES_DIR])
+  end
+end
